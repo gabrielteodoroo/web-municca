d63c6a1a09166176fea122c5629cdd8275d59a26
Teodoro <biel.teodorob@gmail.com>
2024-10-06 21:15:13 -0300

    feat: Integrate Chakra UI, React Query, and Sonner Toaster in main application

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
+import { router } from "./routes";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
